9d371f418ef37dafde204adcd675124b53fed07b
animesh <royanimesh2211@gmail.com>
2020-04-18 15:39:28 +0530

    Handling error cases for all fragments

--- app/src/main/java/com/raywenderlich/android/w00tze/repository/RemoteRepository.kt
+++ app/src/main/java/com/raywenderlich/android/w00tze/repository/RemoteRepository.kt
@@ -3,9 +3,7 @@ package com.raywenderlich.android.w00tze.repository
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
 import com.raywenderlich.android.w00tze.app.Injection
-import com.raywenderlich.android.w00tze.model.Gist
-import com.raywenderlich.android.w00tze.model.Repo
-import com.raywenderlich.android.w00tze.model.User
+import com.raywenderlich.android.w00tze.model.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -15,24 +13,27 @@ object RemoteRepository : Repository {
     private const val TAG = "RemoteRepository"
 
     // user name that we are fetching data from
-//    private const val LOGIN = "animeshroydev"
+    // private const val LOGIN = "animeshroydev"
+    // private const val LOGIN = "animeshroydev"
     private const val LOGIN = "w00tze"
 
     private val api = Injection.provideGithubApi()
 
-    override fun getRepos(): LiveData<List<Repo>> {
-        val liveData = MutableLiveData<List<Repo>>()
+    override fun getRepos(): LiveData<Either<List<Repo>>> {
+        val liveData = MutableLiveData<Either<List<Repo>>>()
 
         api.getRepos(LOGIN).enqueue(object : Callback<List<Repo>> {
 
             override fun onResponse(call: Call<List<Repo>>, response: Response<List<Repo>>?) {
-                if (response != null) {
-                    liveData.value = response.body()
+                if (response != null && response.isSuccessful) {
+                    liveData.value = Either.success(response.body())
+                } else {
+                    liveData.value = Either.error(ApiError.REPO, null)
                 }
             }
 
             override fun onFailure(call: Call<List<Repo>>, t: Throwable) {
-                TODO("Not yet implemented")
+                liveData.value = Either.error(ApiError.REPO, null)
             }
 
         })
@@ -40,19 +41,21 @@ object RemoteRepository : Repository {
         return liveData
     }
 
-    override fun getGists(): LiveData<List<Gist>> {
-        val liveData = MutableLiveData<List<Gist>>()
+    override fun getGists(): LiveData<Either<List<Gist>>> {
+        val liveData = MutableLiveData<Either<List<Gist>>>()
 
         api.getGists(LOGIN).enqueue(object : Callback<List<Gist>> {
 
             override fun onResponse(call: Call<List<Gist>>, response: Response<List<Gist>>?) {
-                if (response != null) {
-                    liveData.value = response.body()
+                if (response != null && response.isSuccessful) {
+                    liveData.value = Either.success(response.body())
+                } else {
+                    liveData.value = Either.error(ApiError.GIST, null)
                 }
             }
 
             override fun onFailure(call: Call<List<Gist>>, t: Throwable) {
-                TODO("Not yet implemented")
+                liveData.value = Either.error(ApiError.GIST, null)
             }
 
         })
@@ -60,21 +63,22 @@ object RemoteRepository : Repository {
         return liveData
     }
 
-    override fun getUser(): LiveData<User> {
-        val liveData = MutableLiveData<User>()
+    override fun getUser(): LiveData<Either<User>> {
+        val liveData = MutableLiveData<Either<User>>()
 
         api.getUser(LOGIN).enqueue(object : Callback<User> {
 
             override fun onResponse(call: Call<User>, response: Response<User>?) {
-                if (response != null) {
-                    liveData.value = response.body()
+                if (response != null && response.isSuccessful) {
+                    liveData.value = Either.success(response.body())
+                } else {
+                    liveData.value = Either.error(ApiError.USER, null)
                 }
 
             }
             override fun onFailure(call: Call<User>, t: Throwable) {
-                TODO("Not yet implemented")
+                liveData.value = Either.error(ApiError.USER, null)
             }
-
         })
 
         return liveData
--- app/src/main/java/com/raywenderlich/android/w00tze/repository/Repository.kt
+++ app/src/main/java/com/raywenderlich/android/w00tze/repository/Repository.kt
@@ -32,13 +32,15 @@
 package com.raywenderlich.android.w00tze.repository
 
 import android.arch.lifecycle.LiveData
+import com.raywenderlich.android.w00tze.model.Either
 import com.raywenderlich.android.w00tze.model.Gist
 import com.raywenderlich.android.w00tze.model.Repo
 import com.raywenderlich.android.w00tze.model.User
 
 interface Repository {
-  fun getRepos(): LiveData<List<Repo>>
-  fun getGists(): LiveData<List<Gist>>
-  fun getUser(): LiveData<User>
+  // Either for error handling
+  fun getRepos(): LiveData<Either<List<Repo>>>
+  fun getGists(): LiveData<Either<List<Gist>>>
+  fun getUser(): LiveData<Either<User>>
 }
 
